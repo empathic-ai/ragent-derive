274d70b7ec14de13e8c2aa669a1ac595023da78d
Catchawink <threeeasywords@gmail.com>
2024-11-24 14:46:11 -0500

    Updating files.

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@ extern crate proc_macro;
 extern crate syn;
 #[macro_use]
 extern crate quote;
+extern crate proc_macro_crate;
 
-use ragent_core::prelude::*;
 use proc_macro::TokenStream;
+use proc_macro_crate::{crate_name, FoundCrate};
 
 #[proc_macro_derive(Task)]
 pub fn task(input: TokenStream) -> TokenStream {
@@ -14,10 +15,21 @@ pub fn task(input: TokenStream) -> TokenStream {
     // Parse the string representation
     let ast: syn::DeriveInput = syn::parse_str(&s).unwrap();
 
+    // Determine the correct path to the ragent crate
+    let found_crate = crate_name("ragent").expect("ragent is not found in Cargo.toml");
+
+    let path = match found_crate {
+        FoundCrate::Itself => quote!(crate::prelude::Task),
+        FoundCrate::Name(crate_name) => {
+            let ident = syn::Ident::new(&crate_name, proc_macro2::Span::call_site());
+            quote!(::#ident::prelude::Task)
+        }
+    };
+
     // Build the impl
     let name = &ast.ident;
     let gen = quote! {
-        impl ragent_core::prelude::Task for #name {
+        impl #path for #name {
         }
     };
     TokenStream::from(gen)
